0b1fe969c20c2b834acb34cc42223126daa427aa
Andrii <rura.andrew@gmail.com>
2016-10-11 09:29:45 +0000

    add new file

--- js/index.js
+++ js/index.js
@@ -35,10 +35,3 @@
         });
       }
       
-      
- $(function() {
-	$('a[href*=#]').on('click', function(e) {
-		e.preventDefault();
-		$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-	});
-});

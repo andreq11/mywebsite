23ca796be9ffc140498bb5f99b89de3bf2a96b91
Andrii <rura.andrew@gmail.com>
2016-10-14 21:15:50 +0000

    add new file

--- js/index.js
+++ js/index.js
@@ -1,37 +1,40 @@
- $(function () {
-    $('#send').click(function(e) {
-        e.preventDefault();
-        $.ajax({
-            url: "https://formspree.io/rura.andrew@gmail.com", 
-            method: "POST",
-            data: {
-                message: $('#name').val(),
-                email: $('#email').val(),
-                text: $('#messages').val()
-            },
-            dataType: "json"
-        }).done(function() {
-            $('form').html('<h1>Thank you!</h1>')
-        }).fail(function(xhr, err) {
-            $('form').html(xhr.statusText);
-        });        
-    })
-});
- 
- 
- 
- 
- 
+ $(function() {
+     $('#send').click(function(e) {
+         e.preventDefault();
+         $.ajax({
+             url: "https://formspree.io/rura.andrew@gmail.com",
+             method: "POST",
+             data: {
+                 message: $('#name').val(),
+                 email: $('#email').val(),
+                 text: $('#messages').val()
+             },
+             dataType: "json"
+         }).done(function() {
+             $('form').html('<h1>Thank you!</h1>')
+         }).fail(function(xhr, err) {
+             $('form').html(xhr.statusText);
+         });
+     })
+ });
+
+
+
+
+
 
  function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-      }
-      
+     var uluru = {
+         lat: -25.363,
+         lng: 131.044
+     };
+     var map = new google.maps.Map(document.getElementById('map'), {
+         zoom: 4,
+         center: uluru
+     });
+     var marker = new google.maps.Marker({
+         position: uluru,
+         map: map
+     });
+ }
+ 
